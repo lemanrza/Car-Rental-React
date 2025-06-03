53c116e4357cf008c49f0c91e877771c450ff56a
lemanrza <lemaner@code.edu.az>
2025-06-03 10:44:28 +0400

    feat: BASE_URL changed

--- src/pages/admin/ManageRentals/index.tsx
+++ src/pages/admin/ManageRentals/index.tsx
@@ -6,7 +6,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Eye } from "lucide-react";
 import { useEffect, useState } from "react";
